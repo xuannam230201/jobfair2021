c3d18d3eb93419dd3a3880306fb6e335b8a0f5ec
Qila8nguyen <otokhung1805@gmail.com>
2023-03-16 01:23:05 +0700

    add organizer list in mongodb

--- client/src/utils/constant.js
+++ client/src/utils/constant.js
@@ -2,10 +2,7 @@ export const PINK_BG = '#F7C8E0'
 export const DARK_BLUE = '#336699'
 export const BLUE_BG = '#B4E4FF'
 export function isOrganizer(itemData) {
-  return (
-    itemData.surname === itemData.surname.toUpperCase() &&
-    !itemData.id.includes('TN')
-  )
+  return itemData.isOrganizer
 }
 
 export const stringName = (item) => {
--- server/app.js
+++ server/app.js
@@ -1,62 +1,62 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const cors = require('cors');
-var mongoose = require('mongoose');
-const SocketServer = require('./socketServer');
-require('dotenv').config();
+var createError = require('http-errors')
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
+const cors = require('cors')
+var mongoose = require('mongoose')
+const SocketServer = require('./socketServer')
+require('dotenv').config()
 
-var app = express();
+var app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade')
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use(cors({
-  origin: 'http://localhost:3000'
-}));
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  })
+)
 
 mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
-
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-http.listen(8000);
-
-io.on('connection', socket => {
-  SocketServer(socket);
 })
 
-var studentsRouter = require('./routes/students');
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+http.listen(8000)
 
-app.use('/students', studentsRouter);
+io.on('connection', (socket) => {
+  SocketServer(socket)
+})
+
+var studentsRouter = require('./routes/students')
+
+app.use('/students', studentsRouter)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use(function (req, res, next) {
+  next(createError(404))
+})
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  res.status(err.status || 500)
+  res.render('error')
+})
 
-
-
-module.exports = app;
+module.exports = app
